cde4e6fb2de88d61a39c8d137e538f3a948dc2cf
Giacomo Benati <giacomobenati@mailbox.org>
2019-03-11 09:57:45 +0100

    added some action types + createad components files

---
+++ src/states/actionTypes.js
@@ -0,0 +1,8 @@
+export const SPINNER_ON = "SPINNER_ON";
+export const SPINNER_OFF = "SPINNER_OFF";
+export const GET_PRISONS = "GET_PRISONS";
+export const ADD_PRISONER = "ADD_PRISONER";
+export const DELETE_PRISONER = "DELETE_PRISONER";
+export const EDIT_PRISONER = "DELETE_PRISONER";
+export const LOGIN_SUCCES = "LOGIN_SUCCES";
+export const ERROR = "ERROR";
